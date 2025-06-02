a66d08f3dac96fe11ee9e672b4b6c3bbc941da3d
Rallentando456 <144925979+raulaine@users.noreply.github.com>
2025-06-02 07:48:53 -0300

    parece bom

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
     
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    const alternaContraste = document.getElementById('alterna-contraste');
-    
+    const alternaContraste = document.getElementById('alterna-contraste')
+
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
@@ -21,6 +21,9 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
+    alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ })
 })
